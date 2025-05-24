7b18060a10d457c70d804a1e45e9ef562777f542
OPericao <lucaspi2003@gmail.com>
2025-05-24 04:54:46 +0200

    Banner de información de turno cambia de color según el equipo que tenga el turno

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -370,7 +370,7 @@ function filtrarCampeones() {
 
 return (
   <div id="appContainer" className="d-flex flex-column min-vh-100">
-    <div className="alert alert-danger text-center fw-bold" role="alert">
+    <div className={`alert ${ side.replace('ban', '') == "blue" ? 'alert-primary' : 'alert-danger'} text-center fw-bold`} role="alert">
       Turno del equipo {side.replace('ban', '')} - Tiempo restante: {tiempoRestante}s
     </div>
     <div className="grid-container" style={{ display: 'grid', gridTemplateColumns: '1.5fr 3fr 1.5fr' }}>
